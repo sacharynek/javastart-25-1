f7c080cbd2c513b79b495f21ce319c340fd053b4
tomaszlach <tomaszlach86@gmail.com>
2019-07-29 14:50:55 +0200

    Initial commit

---
+++ src/main/java/javastart/javastart251/Javastart251Application.java
@@ -0,0 +1,13 @@
+package javastart.javastart251;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javastart251Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Javastart251Application.class, args);
+    }
+
+}
