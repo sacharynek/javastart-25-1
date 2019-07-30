ea394398735fdf872cbce74b2e35df5dbf27973c
tomaszlach <tomaszlach86@gmail.com>
2019-07-30 12:28:57 +0200

    final solution - quite shitty looking but works

--- src/main/java/javastart/javastart251/TransactionController.java
+++ src/main/java/javastart/javastart251/TransactionController.java
@@ -3,9 +3,13 @@ package javastart.javastart251;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.math.BigDecimal;
 import java.sql.Date;
+import java.util.List;
 
 import javax.money.Monetary;
 
@@ -28,6 +32,10 @@ public class TransactionController {
     @GetMapping("/all")
     public String displayWithPaginationController(Model model) {
 
+        List<Transaction> transactions = transactionDao.getAllTransactions();
+        model.addAttribute("transactions", transactions);
+
+
         return "DisplayWithPagination";
     }
 
@@ -37,29 +45,39 @@ public class TransactionController {
         return "Add";
     }
 
+    @GetMapping("/Delete/{id}")
+    public String delete(Model model, @PathVariable(value="id") String id) {
+        transactionDao.deleteByID(Long.parseLong(id));
+        return "redirect:/all";
+    }
+
     @RequestMapping("/addProduct")
     public String addProduct(Model model, @RequestParam() String ttype,
             @RequestParam() String description,
             @RequestParam String amount,
             @RequestParam Date date) {
+            Transaction transaction = new Transaction();
+            transaction.setDescription(description);
+            transaction.setDate(date);
+            transaction.setAmount(new BigDecimal(amount));
+            transaction.setTtype(ttype.equals("true"));
+
+       getTransactionDao().insert(transaction);
 
 
-       getTransactionDao().insert(new Transaction());
-
-
-        return "redirect:/DisplayAll";
-    }
-
-    @GetMapping("/DisplayAll")
-    public String displayAll(Model model) {
-
-
-
-
-
-        return "Index";
+        return "redirect:/all";
     }
 
 
+
+    @GetMapping("/DisplayDetails/{transactionID}")
+    public String displayDetails(Model model, @PathVariable(value="transactionID") String id){
+
+        Transaction transaction = transactionDao.findByID(Long.parseLong(id));
+
+
+        model.addAttribute("transaction", transaction);
+        return "DisplayDetails";
+    }
 
 }
